8268ba7a0da3568857dc1db5d3dcf36c4e571f84
anas benaissa <abenaissa@liadtech.com>
2025-07-26 01:30:08 +0100

    fix issues

--- src/app/view/pages/document-viewer/document-viewer.component.ts
+++ src/app/view/pages/document-viewer/document-viewer.component.ts
@@ -1,39 +1,50 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DocumentService } from '../../../services/document/document.service';
 import { Document } from '../../../models/document.model';
 import { CommonModule } from '@angular/common';
 import { SafeUrlPipe } from '../../../services/safeUrlPipe';
+import { environment } from '../../../../environments/environments';
 
 @Component({
   selector: 'app-document-viewer',
   standalone: true,
-  imports: [CommonModule , SafeUrlPipe],
+  imports: [CommonModule, SafeUrlPipe],
   templateUrl: './document-viewer.component.html',
   styleUrls: ['./document-viewer.component.scss']
 })
-export class DocumentViewerComponent implements OnInit {
+export class DocumentViewerComponent implements OnInit, OnDestroy {
   documentService = inject(DocumentService);
   route = inject(ActivatedRoute);
   doc: Document | null = null;
 
   ngOnInit(): void {
-     document.addEventListener('contextmenu', this.disableRightClick);
+    document.addEventListener('contextmenu', this.disableRightClick);
     document.addEventListener('keydown', this.disableShortcuts);
+
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.documentService.fetchDocumentById(id).subscribe({
-      next: (data) => this.doc = data,
+      next: (data: Document) => {
+        // ✅ Corrige l'URL uniquement si elle commence par "/"
+        if (data.fileUrl?.startsWith('/')) {
+          data.fileUrl = `${environment.apiUrl.replace('/api', '')}${data.fileUrl}`;
+        }
+        this.doc = data;
+      },
       error: () => console.error("Erreur lors du chargement du document")
     });
   }
+
   disableRightClick = (e: MouseEvent) => e.preventDefault();
+
   disableShortcuts = (e: KeyboardEvent) => {
-  if (e.ctrlKey && ['s', 'p', 'u'].includes(e.key.toLowerCase())) {
-    e.preventDefault();
+    if (e.ctrlKey && ['s', 'p', 'u'].includes(e.key.toLowerCase())) {
+      e.preventDefault();
+    }
+  };
+
+  ngOnDestroy() {
+    document.removeEventListener('contextmenu', this.disableRightClick);
+    document.removeEventListener('keydown', this.disableShortcuts);
   }
-};
-ngOnDestroy() {
-  document.removeEventListener('contextmenu', this.disableRightClick);
-  document.removeEventListener('keydown', this.disableShortcuts);
-}
 }
